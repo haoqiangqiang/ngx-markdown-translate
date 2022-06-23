79180a397168381f43c4115c63c17ffa193ca14a
haoqiangqiagn <826173335@qq.com>
2022-06-23 21:23:49 +0800

    添加各模块

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AppCheatSheetComponent } from './pages/cheatSheet/cheat-sheet.component';
+import { AppHomeComponent } from './pages/home/home.component';
+import { AppPluginsComponent } from './pages/plugins/plugins.component';
+import { AppProblemComponent } from './pages/problem/problem.component';
+import { AppReRenderComponent } from './pages/reRender/re-render.component';
+import { AppSyntaxHighlightComponent } from './pages/syntaxHighlight/syntax-highlight.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppCheatSheetComponent,
+    AppHomeComponent,
+    AppPluginsComponent,
+    AppProblemComponent,
+    AppReRenderComponent,
+    AppSyntaxHighlightComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/syntaxHighlight/syntax-highlight.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-syntax-highlight',
+    templateUrl: './syntax-highlight.component.html',
+    styleUrls: ['./syntax-highlight.component.scss'],
+})
+export class AppSyntaxHighlightComponent {
+    
+}
